03e7cbbe7c191969e5fd4c9d596aa45fecf59ff0
Wjatscheslaw Poluschin(Kosh) <wjatscheslaw@poluschin.com>
2024-04-01 17:32:38 +0200

    Refactor database backup script and implement full backup functionality
    The backup script has been significantly refactored to improve readability and maintainability. It now primarily focuses on conducting a full backup, which includes creating a backup directory, backing up data using MariaDB, compressing the backup into a tarball, and uploading it to an S3 bucket. The use of environment variables has also been incorporated for better customization. Previous message-related portions of the code have been commented out for future improvements and implementation of incremental backup functionality.

--- resources/perform-backup.sh
+++ resources/perform-backup.sh
@@ -1,64 +1,198 @@
-#/bin/sh
-
-
-# Set the has_failed variable to false. This will change if any of the subsequent database backups/uploads fail.
-has_failed=false
-backup_name=""
-
-
-# Loop through all the defined databases, seperating by a ,
-for CURRENT_DATABASE in ${TARGET_DATABASE_NAMES//,/ }
-do
-
-    # Perform the database backup. Put the output to a variable. If successful upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
-    if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $CURRENT_DATABASE 2>&1 > /tmp/$CURRENT_DATABASE.sql)
-    then
-
-        echo -e "Database backup successfully completed for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
-        backup_name=$CURRENT_DATABASE_$(date +"%Y-%m-%d_%H-%M-%S").sql
-        # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
-        if awsoutput=$(aws s3 cp /tmp/$CURRENT_DATABASE.sql $AWS_BUCKET_URI$AWS_BUCKET_BACKUP_PATH/$backup_name 2>&1)
-        then
-            echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
-        else
-            echo -e "Database backup failed to upload for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $awsoutput" | tee -a /tmp/kubernetes-s3-mysql-backup.log
-            has_failed=true
-        fi
-
-    else
-        echo -e "Database backup FAILED for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $sqloutput" | tee -a /tmp/kubernetes-s3-mysql-backup.log
-        has_failed=true
-    fi
-
-done
-
-
-
-# Check if any of the backups have failed. If so, exit with a status of 1. Otherwise exit cleanly with a status of 0.
-if [ "$has_failed" = true ]
-then
-
-    # If Slack alerts are enabled, send a notification alongside a log of what failed
-    if [ "$NOTIFY_ENABLED" = true ]
-    then
-        # Put the contents of the database backup logs into a variable
-        logcontents=`cat /tmp/kubernetes-s3-mysql-backup.log`
-
-        # Send Slack alert
-        /notify.sh "One or more backups on database host $TARGET_DATABASE_HOST failed. The error details are included below:" "$logcontents"
-    fi
-
-    echo -e "kubernetes-s3-mysql-backup encountered 1 or more errors. Exiting with status code 1."
-    exit 1
-
-else
-
-    # If Slack alerts are enabled, send a notification that all database backups were successful
-    if [ "$NOTIFY_ENABLED" = true ]
-    then
-        /notify.sh "Backup created: $backup_name"
-    fi
-
-    exit 0
-
+#!/usr/bin/env bash
+BACKUP_DIR=$(dirname "$0")
+DATE=$(date +%Y-%m-%d-%H%M)
+if [ -z "$BACKUP_TYPE" ]; then
+    BACKUP_TYPE=full
 fi
+
+if [ $BACKUP_TYPE = "full" ]; then
+    TARGET_NAME_PREFIX=full
+else
+    TARGET_NAME_PREFIX=incremental
+fi
+
+cd "${BACKUP_DIR}" || exit
+
+TARGET_NAME_FULL=${TARGET_NAME_PREFIX}.full
+TARGET_NAME_INCREMENTAL=${TARGET_NAME_PREFIX}_${DATE}.inc
+
+echo "Starting backup..."
+echo $TARGET_NAME_FULL
+
+full_backup() {
+  START=$(date +%s)
+  echo "Start full backup at ${START}"
+    if [ -d ./${TARGET_NAME_FULL} ]; then
+      rm -rf ./${TARGET_NAME_FULL}
+    fi
+    mkdir -p ./${TARGET_NAME_FULL}
+    mariadb-backup --backup \
+        --target-dir=./${TARGET_NAME_FULL}/data/ \
+        --host="${DB_HOST}" \
+        --port=3306 \
+        --user="${DB_USER}" \
+        --password="${DB_PASSWORD}" \
+        2>&1 | tee -a ./${TARGET_NAME_FULL}/${TARGET_NAME_PREFIX}.log
+
+    tar -cpzf ./${TARGET_NAME_FULL}.tar.gz ${TARGET_NAME_FULL}
+    s5cmd cp ./${TARGET_NAME_FULL}.tar.gz "s3://${BUCKET_NAME}/${BACKUP_PREFIX}/${TARGET_NAME_FULL}.tar.gz"
+    rm -rf ${TARGET_NAME_FULL}.tar.gz
+    END=$(date +%s)
+    echo "End full backup at ${END}"
+}
+
+full_backup
+
+#incremental_backup() {
+#    if [ ! -d "${TARGET_NAME_FULL}/data" ]; then
+#        echo "No full backup found. Sync from s3"
+#        s5cmd cp "s3://${S3_BUCKET}/${S3_PATH}/${TARGET_NAME_FULL}.tar.gz" ./
+#    fi
+#
+#    if [ -d "./${TARGET_NAME_INCREMENTAL}" ]; then
+#        rm -rf "./${TARGET_NAME_INCREMENTAL}"
+#    fi
+#
+#    mkdir -p "./${TARGET_NAME_INCREMENTAL}"
+#
+#    echo "Start mariabackup utility..."
+#
+#    START=$(date +%s)
+#
+#    # Execute backup of db. Change options and credentials for mariabackup here
+#    mariadb-backup --backup \
+#        --target-dir=./${TARGET_NAME_INCREMENTAL}/data/ \
+#        --incremental-basedir=${INCREMENTAL_BASE_DIR}/data/ \
+#        --host="${DB_HOST}" \
+#        --port=3306 \
+#        --user="${DB_USER}" \
+#        --password="${DB_PASSWORD}" \
+#        2>&1 | tee -a ./${TARGET_NAME_INCREMENTAL}/${TARGET_NAME_PREFIX}.log
+#}
+#
+
+#
+#
+#
+#case ${ACTION} in
+#    FULL)
+#        if [ -d ./${TARGET_NAME_FULL} ]
+#        then
+#            printf "Directory ${BACKUP_DIR}/${TARGET_NAME_FULL} already exists. No full backup was created!\n"
+#            exit 3
+#        fi
+#
+#        mkdir -p ./${TARGET_NAME_FULL}
+#
+#        printf "Start mariabackup utility...\n"
+#
+#        START=$(date +%s)
+#
+#        # Execute backup of db. Change options and credentials for mariabackup here
+#        mariabackup --backup \
+#            --target-dir=./${TARGET_NAME_FULL}/data/ \
+#            --host=127.0.0.1 \
+#            --port=3306 \
+#            --user=backup \
+#            >> ./${TARGET_NAME_FULL}/${TARGET_NAME_PREFIX}.log 2>&1
+#
+#        BACKUP_RETURNSTATE=$?
+#        END=$(date +%s)
+#
+#        printf "\n----------------------------------------\n\n" >> ./${TARGET_NAME_FULL}/${TARGET_NAME_PREFIX}.log
+#        printf "Execution date:      ${DATE}\n" >> ./${TARGET_NAME_FULL}/${TARGET_NAME_PREFIX}.log
+#        printf "Backup runtime:      $((END-START))s\n" >> ./${TARGET_NAME_FULL}/${TARGET_NAME_PREFIX}.log
+#
+#        if [ ${BACKUP_RETURNSTATE} -ne 0 ]
+#        then
+#            printf "Failed to create full backup. Check ${BACKUP_DIR}/${TARGET_NAME_FULL}/${TARGET_NAME_PREFIX}.log\n"
+#            exit 5
+#        fi
+#
+#        printf "Full backup created in: ${BACKUP_DIR}/${TARGET_NAME_FULL}\n"
+#        printf "Compressing files...\n"
+#
+#        # Pack new backup files to .tar.gz
+#        tar -cpzf ${TARGET_NAME_FULL}.tar.gz ${TARGET_NAME_FULL}
+#
+#        # Remove folders with full backup except newly created folder
+#        find ./${TARGET_NAME_PREFIX}_*.full -maxdepth 0 -type d -not -name ${TARGET_NAME_FULL} -exec rm -r {} \;
+#
+#        printf "Tar-Archive created: ${BACKUP_DIR}/${TARGET_NAME_FULL}.tar.gz.\n"
+#        ;;
+#    INCREMENTAL)
+#        if [ -z ${INCREMENTAL_BASE_DIR} ] || [ ! -d ${INCREMENTAL_BASE_DIR} ]
+#        then
+#            printf "No base dir for incremental backup found\n"
+#            exit 4
+#        fi
+#
+#        if [ -d ./${TARGET_NAME_INCREMENTAL} ]
+#        then
+#            printf "Directory ${BACKUP_DIR}/${TARGET_NAME_INCREMENTAL} already exists. No incremental backup was created!\n"
+#            exit 3
+#        fi
+#
+#        mkdir -p ./${TARGET_NAME_INCREMENTAL}
+#
+#        printf "Start mariabackup utility...\n"
+#
+#        START=$(date +%s)
+#
+#        # Execute backup of db. Change options and credentials for mariabackup here
+#        mariabackup --backup \
+#            --target-dir=./${TARGET_NAME_INCREMENTAL}/data/ \
+#            --incremental-basedir=${INCREMENTAL_BASE_DIR}/data/ \
+#            --host=127.0.0.1 \
+#            --port=3306 \
+#            --user=backup \
+#            >> ./${TARGET_NAME_INCREMENTAL}/${TARGET_NAME_PREFIX}.log 2>&1
+#
+#        BACKUP_RETURNSTATE=$?
+#        END=$(date +%s)
+#
+#        printf "\n----------------------------------------\n\n" >> ./${TARGET_NAME_INCREMENTAL}/${TARGET_NAME_PREFIX}.log
+#        printf "Execution date:      ${DATE}\n" >> ./${TARGET_NAME_INCREMENTAL}/${TARGET_NAME_PREFIX}.log
+#        printf "Backup runtime:      $((END-START))s\n" >> ./${TARGET_NAME_INCREMENTAL}/${TARGET_NAME_PREFIX}.log
+#        printf "Incremental basedir: `basename ${INCREMENTAL_BASE_DIR}`\n" >> ./${TARGET_NAME_INCREMENTAL}/${TARGET_NAME_PREFIX}.log
+#
+#        if [ ${BACKUP_RETURNSTATE} -ne 0 ]
+#        then
+#            printf "Failed to create incremental backup. Check ${BACKUP_DIR}/${TARGET_NAME_INCREMENTAL}/${TARGET_NAME_PREFIX}.log\n"
+#            exit 5
+#        fi
+#
+#        printf "Incremetal backup created in: ${BACKUP_DIR}/${TARGET_NAME_INCREMENTAL}\nBase dir used: ${INCREMENTAL_BASE_DIR}\n"
+#        printf "Compressing files...\n"
+#
+#        # Pack new backup files to .tar.gz
+#        tar -cpzf ${TARGET_NAME_INCREMENTAL}.tar.gz ${TARGET_NAME_INCREMENTAL}
+#
+#        # Remove folders with incremental backup
+#        find ./${TARGET_NAME_PREFIX}_*.inc -maxdepth 0 -type d -exec rm -r {} \;
+#
+#        printf "Tar-Archive created: ./${TARGET_NAME_INCREMENTAL}.tar.gz and folder removed.\n"
+#        ;;
+#esac
+#
+## Copy all backuped files to remote location
+## Files are defined in temporary .export file
+#
+#printf "Move backups to storage destination...\n"
+#
+#find ./${TARGET_NAME_PREFIX}_*.tar.gz -maxdepth 0 -type f | sort -nr > ./.export
+#rsync --times --protect-args --remove-source-files --files-from=./.export . ${STORAGE_DEST}
+#
+#RSYNC_RETURNSTATE=$?
+#
+## Remove temporary .export file
+#rm ./.export
+#
+#if [ ${RSYNC_RETURNSTATE} -ne 0 ]
+#then
+#    printf "Failed to move (some) backups to ${STORAGE_DEST}\n"
+#    exit 6
+#else
+#    printf "Backups moved to ${STORAGE_DEST}\n"
+#    exit 0
+#fi
