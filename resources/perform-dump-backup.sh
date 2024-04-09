f261937a5ccde93f3683f55928910db252243fd3
Wjatscheslaw Poluschin(Kosh) <wjatscheslaw@poluschin.com>
2024-04-09 22:05:17 +0200

    fix: Refine database backup script and add compression
    Modified the perform-dump-backup.sh script to create individual directories for each database backup, before compressing them into a tar.gz file. Also incorporated SHA256 checksum generation for the backup file and synchronization with an S3 bucket. After completing the backup, cleanup process is also initiated.

--- resources/perform-dump-backup.sh
+++ resources/perform-dump-backup.sh
@@ -12,10 +12,17 @@ fi
 
 
 # Use the mysql command to fetch all database names
-dblist=$($db_cmd -h "${DB_HOST}" "${DB_USER}" -p"${DB_PASSWORD}"  -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|sys|innodb|mysql)")
+dblist=$($db_cmd -h "${DB_HOST}" -u"${DB_USER}" -p"${DB_PASSWORD}"  -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|sys|innodb|mysql)")
 
 for db in $dblist; do
     echo "Backing up database: $db"
-    $dmp_cmd -u"${DB_USER}" -p"${DB_PASSWORD}" -a -B --replace --skip-add-drop-table --no-data  "$db" > "${BACKUP_DIR}/${db}-${DATE}-structure.sql"
-    $dmp_cmd -u"${DB_USER}" -p"${DB_PASSWORD}" -a -B --replace --insert-ignore --no-create-info  "$db" > "${BACKUP_DIR}/${db}-${DATE}-data.sql"
+    mkdir -p "${BACKUP_DIR}/${db}/${DATE}"
+    $dmp_cmd -h "${DB_HOST}" -u"${DB_USER}" -p"${DB_PASSWORD}" -a -B --replace --skip-add-drop-table --no-data  "$db" > "${BACKUP_DIR}/${db}/${DATE}/structure.sql"
+    $dmp_cmd -h "${DB_HOST}" -u"${DB_USER}" -p"${DB_PASSWORD}" -a -B --replace --insert-ignore --no-create-info  "$db" > "${BACKUP_DIR}/${db}/${DATE}/data.sql"
+    tar -czf "${BACKUP_DIR}/${db}/${DATE}.tar.gz" "${BACKUP_DIR}/${db}/${DATE}"
+    sha256sum "${BACKUP_DIR}/${db}/${DATE}.tar.gz" > "${BACKUP_DIR}/${db}/${DATE}.tar.gz.sha256"
+    # shellcheck disable=SC2115
+    rm -rf "${BACKUP_DIR}/${db}/${DATE}"
 done
+s5cmd sync . "s3://${BUCKET_NAME}/${BACKUP_PREFIX}/"
+rm ./* -rf
